f1f89470bb35a10f4e63748d133c963870637f4c
Sumit Das <sumit8das@gmail.com>
2019-04-06 17:09:29 +0530

    Clean Code

--- 2-MakeWords4mLists.py
+++ 2-MakeWords4mLists.py
@@ -2,21 +2,7 @@
 # Sumit Das
 
 import enchant
-
-
-# f = open("WORDS.TXT", "r")
-
 D = enchant.Dict("en_US")
-
-"""for line in f:
-	myLINE = f.readline()
-	# print(f.readline())
-	# print(line)
-	# myCHECK = D.check(myLINE)
-	# print(myCHECK)
-	# if myCHECK == True:
-	#   print(line)
-	print(D.suggest(line))"""
 
 with open('WORDS.TXT') as f:
     lines = f.read().splitlines()
--- 3-RunMe.cmd
+++ 3-RunMe.cmd
@@ -1,6 +1,17 @@
 @echo off
-cd C:\Users\in10c2\Documents\MEGAsync\Letters2Words
 
-g++ WORDS.cpp -o WORDS.exe
-WORDS.exe > WORDS.TXT
-python WORDS.py
+REM Sumit Das
+REM 2019-04-06
+
+REM REM your path to master    cd C:\Users\in10c2\Documents\MEGAsync\Letters2Words
+REM Provide the series of letters without any space
+
+REM Complie your CPP program
+REM You can remove this once the WORDS.exe file is created
+g++ 1-MakeWordList4mLetters.cpp -o WORDS.exe
+
+REM Run the compiled CCP program to get list of works
+WORDS.exe %1 > WORDS.TXT
+
+REM run the Python script to generate words from the list using enchant
+python 2-MakeWords4mLists.py
