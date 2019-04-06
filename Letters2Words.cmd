9b1dd0609fd1736f42f58504f1e4a8ccac05fe65
Sumit Das <sumit8das@gmail.com>
2019-04-06 18:11:26 +0530

    Added Easier to Use Command-Linde Script

---
+++ Letters2Words.cmd
@@ -0,0 +1,18 @@
+@echo off
+
+REM Sumit Das
+REM 2019-04-06
+
+REM REM your path to master    
+cd C:\Users\in10c2\Documents\MEGAsync\Letters2Words
+REM Provide the series of letters without any space
+
+REM Complie your CPP program
+REM You can remove this once the WORDS.exe file is created
+REM g++ 1-MakeWordList4mLetters.cpp -o WORDS.exe
+
+REM Run the compiled CCP program to get list of works
+WORDS.exe %1 > WORDS.TXT
+
+REM run the Python script to generate words from the list using enchant
+python 2-MakeWords4mLists.py
